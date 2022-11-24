c85253341b84c9fe6c8ff169c928c63ab7d2415d
Mustafa Akkas <mstf.akkas.2000@gmail.com>
2022-11-25 00:30:46 +0300

    Toolbar and hero completed

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@mui/material";
+
+import { PhotoCamera } from "@material-ui/icons";
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="InfoText" gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant="h5" align="center" color={"MenuText"} paragraph>
+              Hello everyone this is a photo album and I'm trying to make this
+              sentence as long as possible so we can see how does it look like
+              on the screen
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
